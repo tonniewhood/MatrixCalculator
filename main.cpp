d8fec2e837e48c33caf0c5b8103509606d7dfdb6
Anthony Wood <woodanthony15@gmail.com>
2023-12-04 19:36:24 -0700

    secondary status update

--- main.cpp
+++ main.cpp
@@ -14,7 +14,80 @@ int main() {
         cout << ">>";
         cin >> input;
 
-        string operation = parse_input(input);
+        inputResponse response = parseInput(input);
+
+        // Determine if there was a syntax error in the use input, if so, tell the user and request a second input
+        if(response.hasError){
+            cout << response.errorMsg << "\n\n";
+            continue;
+        }
+        else {
+            // Perform the operation that corresponds with the operations.txt file
+            switch (response.operation){
+                // Initialize a matrix
+                case (0) : {
+                    
+                }
+                case (1) : {
+
+                }
+                case (2) : {
+
+                }
+                case (3) : {
+
+                }
+                case (4) : {
+
+                }
+                case (5) : {
+
+                }
+                case (6) : {
+                    
+                }
+                case (7) : {
+                    
+                }
+                case (8) : {
+                    
+                }
+                case (9) : {
+                    
+                }
+                case (10) : {
+                    
+                }
+                case (11) : {
+                    
+                }
+                case (12) : {
+                    
+                }
+                case (13) : {
+                    
+                }
+                case (14) : {
+                    
+                }
+                case (15) : {
+                    
+                }
+                case (16) : {
+                    
+                }
+                case (17) : {
+                    
+                }
+                case (18) : {
+                    done = true;
+                    continue;
+                }
+                default : {
+                    cout << "Error. Operation could not be found\n" << endl; 
+                }
+            }
+        }
 
     }
 
